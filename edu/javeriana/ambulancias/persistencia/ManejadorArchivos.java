1086a7890eaf5d3c3a103ad9c6c0fffb8d8a5674
johan-smc <johan_smc@hotmail.com>
2016-03-13 14:37:46 -0500

    GA

--- edu/javeriana/ambulancias/persistencia/ManejadorArchivos.java
+++ edu/javeriana/ambulancias/persistencia/ManejadorArchivos.java
@@ -1,13 +1,15 @@
 package co.edu.javeriana.ambulancias.persistencia;
 import co.edu.javeriana.ambulancias.negocio.*;
+
+import java.io.File;
 import java.util.Scanner;
-import java.util.File;
 
 public class ManejadorArchivos {
 	public void ingresarAmbulancia(String direccion,EmpresaAmbulancias empresaAmbulancia)
 	{
 		
 		Scanner sc=new Scanner(new File("./"+direccion));
+		//GAYYYYYYYYYYYYYYYYY
 		
 	}
 	
